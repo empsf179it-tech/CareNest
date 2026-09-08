aa95d55cf5e2e86e6dbbc9d8b865c57e7c63e624
empsf179it-tech <empsf179it@gmail.com>
2026-09-08 17:29:10 +0530

    SECOND COMMIT

--- assets/js/animations.js
+++ assets/js/animations.js
@@ -14,7 +14,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 scrollTrigger: {
                     trigger: el,
                     start: "top 85%", // reveals when top of element hits 85% of viewport
-                    toggleActions: "play none none reverse"
+                    toggleActions: "play none none none"
                 }
             }
         );
